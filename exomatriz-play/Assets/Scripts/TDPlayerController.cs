fed05a6017721bd2e7cc73a60b1efdded8e4380d
lmorv <kishongus@gmail.com>
2023-11-07 11:13:02 -0500

    Movement: Player movement experiments
    - Testing out some player movement options. Top down and click to move. I ran into issues with the click to move behavior, my guess is that it's got to do whit the way the horizontal plane axis are treated. The player character only moves in a horizontal line across the screen and does not travel up an down to the actual xy coordinates of the target direction. Don't know how to fix it at this moment.
    - I may move on to visual developments aspects of the project, or game loop structural stuff (implementing chapter/ scene transitions), or maybe I'll try to get a particle simulation going.

--- exomatriz-play/Assets/Scripts/TDPlayerController.cs
+++ exomatriz-play/Assets/Scripts/TDPlayerController.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 
 public class TDPlayerController : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet;
+    // [SerializeField] private GameObject bullet;
     [SerializeField] private float movementVelocity = 3f;
-    [SerializeField] private Transform bulletDirection;
+    // [SerializeField] private Transform bulletDirection;
     
     private TDActions controls;
-    private bool canShoot = true;
+    // private bool canShoot = true;
     private Camera main;
 
     private void Awake()
@@ -30,27 +30,27 @@ public class TDPlayerController : MonoBehaviour
     void Start()
     {
         main = Camera.main;
-       controls.Player.Shoot.performed += _ => PlayerShoot(); // Underscore instead of ctx (context value) cause we are not reading a context value in this case.
+       // controls.Player.Shoot.performed += _ => PlayerShoot(); // Underscore instead of ctx (context value) cause we are not reading a context value in this case.
     }
 
-    private void PlayerShoot()
-    {
-        if (canShoot) return;
-        
-        Vector2 mousePosition = controls.Player.MousePosition.ReadValue<Vector2>();
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        GameObject g = Instantiate(bullet, bulletDirection.position, bulletDirection.rotation);
-        g.SetActive(true);
-        StartCoroutine(CanShoot());
+    // private void PlayerShoot()
+    // {
+    //     if (canShoot) return;
+    //     
+    //     Vector2 mousePosition = controls.Player.MousePosition.ReadValue<Vector2>();
+    //     mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    //     GameObject g = Instantiate(bullet, bulletDirection.position, bulletDirection.rotation);
+    //     g.SetActive(true);
+    //     StartCoroutine(CanShoot());
+    //
+    // }
 
-    }
-
-    IEnumerator CanShoot()
-    {
-        canShoot = false;
-        yield return new WaitForSeconds(.5f);
-        canShoot = true;
-    }
+    // IEnumerator CanShoot()
+    // {
+    //     canShoot = false;
+    //     yield return new WaitForSeconds(.5f);
+    //     canShoot = true;
+    // }
     void Update()
     {
         //Rotation
